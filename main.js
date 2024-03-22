800eefe9719acacfc125a6d230971a06fdc0c5b7
Leong Jia Wei, Marcus <54733441+leongjwm@users.noreply.github.com>
2024-03-23 01:07:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
-  var x = document.getElementById("nav-elements");
+  var x = document.getElementById("nav-elements").getElementsByTagName("li");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
