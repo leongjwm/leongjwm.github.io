1924bc2e4b70b71d1ccac29bd6abbdff72170dfc
Leong Jia Wei, Marcus <54733441+leongjwm@users.noreply.github.com>
2024-05-05 00:35:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("nav-elements-mobile");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  } 
+function clickHamburgerMenu() {
+  var x = document.getElementById("nav-elements-mobile");
+  var y = document.getElementsByClassName("mobile")[0];
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+    y.style.height = "auto";
+    document.getElementById("nav-svg").src = "./icons/hamburger-menu.svg"
+  } else {
+    x.style.display = "flex";
+    y.style.height = "100vh";
+    document.getElementById("nav-svg").src = "./icons/cross.svg"
+  }
+} 
